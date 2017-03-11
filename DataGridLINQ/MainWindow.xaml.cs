78c8e95719be78c7d5c6edf328bf94ecde0fb443
campus <campus@iic.md>
2017-03-11 12:57:57 +0200

    first commit

---
+++ DataGridLINQ/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace DataGridLINQ
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ShowMustGoOn_Click(object sender, RoutedEventArgs e)
+		{
+			var constr = ConfigurationManager.ConnectionStrings["constr"].ToString();
+			var dbContext = new DataGridLINQDataContext(constr);
+			var result = from ticket in dbContext.Tickets
+				select new
+				{
+					ticket.FirstName,
+					ticket.SecondName,
+					Source = ticket.City.Name,
+				};
+			dataGrid.ItemsSource = result;
+		}
+		}
+}
